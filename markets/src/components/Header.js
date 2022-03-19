6e16b9b2ce1e1a94514b1873121a4303c386756d
rob49157 <robertopaipo@hotmail.com>
2022-03-19 10:16:09 -0400

    pic

--- markets/src/components/Header.js
+++ markets/src/components/Header.js
@@ -1,18 +1,14 @@
 import React, { useState } from "react";
-import {
-  Navbar,
-  Nav,
-  Container,
-  NavDropdown,
-  Toolbar,
-  Header,
-  PageHeader
-} from "react-bootstrap";
+import { Navbar,Nav,Container,} from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Crypto from "../components/Crypto";
 import Stock from "../components/Stock";
 import Forex from "../components/Forex";
+
+import cryptopic from '../images/crypto.jpg';
+import forexpic from '../images/forex.jpg';
+import stockpic from '../images/stock.jpg';
 
 
 
@@ -27,11 +23,11 @@ const header = (props) => {
       <>
   <Navbar bg="secondary" variant="dark">
     <Container>
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Navbar.Brand href="#home">MarketList</Navbar.Brand>
     <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="#Crypto">Crypto</Nav.Link>
+      <Nav.Link href="#Stock">Stock</Nav.Link>
+      <Nav.Link href="#Forex">Forex</Nav.Link>
     </Nav>
     </Container>
   </Navbar>

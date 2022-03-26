0518d138bb47a0b2205773cd6e83ecb12f9c4e57
rob49157 <robertopaipo@hotmail.com>
2022-03-25 21:20:27 -0400

    coingecko fetching

---
+++ markets/src/Apis/coingecko.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURl:"https://api.coingecko.com/api/v3"
+})
--- markets/src/components/Crypto.js
+++ markets/src/components/Crypto.js
@@ -1,16 +1,29 @@
-import React from "react";
-import axios from "../../package.json"
+import React, { useEffect, useState } from "react";
+import coingecko from "../Apis/coingecko";
+ // const URL="https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=40&page=1&sparkline=false
 
-async function Crypto(){
-    try {
-        const URL="https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=40&page=1&sparkline=false";
-        const {data}= await axios({
-            method:'GET',
-            url:URL
-        })
-    }catch (err){
-        console.log(err)
-} 
-}
+
+ const Crypto= ()=>{
+     const [coins, setcoins]= useState([])
+     useEffect(()=>{
+         const fetchdata= async ()=>{
+            const response= await coingecko.get("/coins/markets",{
+                params:{
+                    vs_currency:"usd",
+                    ids:"bitcoin,rushmoon"
+                 }
+            })
+           
+             console.log(response.data)
+
+         }
+         fetchdata()
+     },[])
+     return <div>
+
+
+     </div>
+ };
+
 
 export default Crypto()

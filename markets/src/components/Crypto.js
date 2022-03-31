615696dd422dbdd6d417f9cb0101658f5628be81
rob49157 <robertopaipo@hotmail.com>
2022-03-30 20:58:27 -0400

    crypto page css

--- markets/src/components/Crypto.js
+++ markets/src/components/Crypto.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import coingecko from "../Apis/coingecko";
 import { ReactDOM } from "react-dom";
 import axios from "axios";
-// const URL="https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=40&page=1&sparkline=false
+import "./crypto.css"
 
 export default function Crypto() {
   const [coins, setCoins] = useState([]);

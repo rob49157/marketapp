99d0b1c8dcbb000d4bcc307e90dcb0faf14cf728
rob49157 <robertopaipo@hotmail.com>
2022-03-30 20:04:00 -0400

    crypto page showing

--- markets/src/components/Crypto.js
+++ markets/src/components/Crypto.js
@@ -7,7 +7,7 @@ import axios from "axios";
 export default function Crypto() {
   const [coins, setCoins] = useState([]);
   // usestate to search for coins
-  const [search, setSearch]=useState('')
+  const [search, setSearch] = useState("");
 
   //useeffect to fetch data from coingecko
   useEffect(() => {
@@ -23,18 +23,18 @@ export default function Crypto() {
 
   // create a handle change function for the searchbar
 
-  const handlechange = e=>{
-    setSearch(e.target.value)
-  }
+  const handlechange = (e) => {
+    setSearch(e.target.value);
+  };
 
   //create function to filter thru coins and display what users types in
 
-  const filteredcoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()))
-
-
+  const filteredcoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   //JSX function destructioring returning data
-  const Coin = ({ name, image, symbol, price, volume }) => {
+  const Coin = ({ name, image, symbol, price, volume, pricechange, marketcap }) => {
     return (
       <div className="coin-container">
         <div className="coin-row">
@@ -44,17 +44,21 @@ export default function Crypto() {
             <p className="coin-symbol">{symbol}</p>
           </div>
           <div className="coin-data">
-            <p className="coin-price">{price}</p>
+            <p className="coin-price">${price}</p>
             <p className="coin-volume">${volume.toLocaleString()}</p>
+            {pricechange < 0 ? (
+              <p className="coin-percent red">{pricechange.toFixed(2)}%</p>
+            ) : (
+              <p className="coin-percent green">{pricechange.toFixed(2)}%</p>
+            )}
+            <p className=" coin-marketcap"> Market cap :$ {marketcap.toLocaleString()}</p>
           </div>
         </div>
-        
       </div>
     );
-  }; 
- 
+  };
 
-  // Displayed JSX
+  // Displayed JSX and Map thru api to retrieve data
   return (
     <div className="Crypto">
       <div className="coin-search">
@@ -68,19 +72,22 @@ export default function Crypto() {
           ></input>
         </form>
       </div>
-      {filteredcoins.map(coin=> {
-          return(
-           <Coin key={coin.id}
-                  name={coin.name}
-                  image={coin.image}
-                  symbol={coin.symbol}
-                  volume={coin.market_cap}
-                  price={coin.current_price}
-           /> 
-             
-          
-          )
-        })}
+      
+      {filteredcoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            marketcap={coin.market_cap}
+            price={coin.current_price}
+            pricechange={coin.price_change_percentage_24h}
+            volume={coin.total_volume}
+
+          />
+        );
+      })}
     </div>
   );
 }

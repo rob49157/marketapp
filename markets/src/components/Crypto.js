95916612c061a2c75a73d8ab877546e81e2c9126
rob49157 <robertopaipo@hotmail.com>
2022-03-30 21:25:57 -0400

    crypto page css

--- markets/src/components/Crypto.js
+++ markets/src/components/Crypto.js
@@ -13,7 +13,7 @@ export default function Crypto() {
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=40&page=1&sparkline=false"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=400&page=1&sparkline=false"
       )
       .then((response) => {
         setCoins(response.data);

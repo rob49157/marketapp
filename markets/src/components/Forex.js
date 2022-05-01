94f789ce3e2b12f125c7a8e9849158f23ea3e155
rob49157 <robertopaipo@hotmail.com>
2022-04-30 22:01:34 -0400

    rates showed up

--- markets/src/components/Forex.js
+++ markets/src/components/Forex.js
@@ -2,47 +2,76 @@ import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import axios from "axios";
 
+
+
 export default function Forex() {
   const [forex, setForex] = useState([]);
   // state for searchbar
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState(" ");
 
-  useEffect(() => {
-    axios
-      .get("https://api.exchangerate.host/latest?base=USD")
-      .then((response) => {
-        setForex(response.data);
-        console.log(response.data);
-      });
-  }, []);
+  useEffect(()=>{
+    getRates()
+  })
 
-  // where the erros begin
-  // const handlechange= (e)=>{
-  //     setSearch(e.target.value)
-  // }
 
-  //   const filtermoney = forex.filter((symbol)=>
-  //   symbol.rates.toLowerCase().includes(search.toLowerCase()))
-
-  <Container>
-    {forex.map((key) => {
-git         
-      return (
-        <div className="forex-container">
-          <div className="forex-row">
-            <div className="forex">
-              <p className="rates" key={key.rates}>
-                {key.rates}fasdfas
-              </p>
-              <p classname="date" key={key.date}>
-                {key.date}
-              </p>
-            </div>
-          </div>
-          
-        </div>
-      );
-      })}
+  const getRates = async (base) =>{
+    const res= await axios.get("https://api.exchangerate.host/latest?base=USD")
+    const { rates } = res.data
     
-  </Container>;
-}
+    const ratesTemp=[]
+    for(const[symbol,rate] of Object.entries(rates)){
+      ratesTemp.push({ symbol, rate})
+    }
+    setForex(ratesTemp)
+  }
+  
+  return(
+    <div>
+      {forex.map((d)=>(
+        <h1 className="forex">{d.symbol}:{d.rate}</h1>
+      
+        
+      ))}
+    </div>
+  )
+
+  // useEffect(()=>{
+  //   axios.get("https://api.exchangerate.host/latest?base=USD")
+  //   .then((response)=>{
+  //     setForex(response.data.rates)
+  //     console.log(response.data)
+
+  //   })
+  //   .catch(error=>console.error(`Error:${error}`))
+
+  // },[])
+  
+
+  // return(
+  //   <div className="fxrates">
+  //     <h1>
+  //    {Object.keys(forex)}
+  //    </h1>
+     
+  //   </div>
+  // )
+
+ 
+
+  
+  
+  
+  
+
+
+  }
+ 
+
+  
+
+
+
+
+      
+    
+  

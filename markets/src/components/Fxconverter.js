a4461848ed7fa7c28b7ff3fa33a806bba3134f4d
rob49157 <robertopaipo@hotmail.com>
2022-05-11 20:22:46 -0400

    fxconverter additions

--- markets/src/components/Fxconverter.js
+++ markets/src/components/Fxconverter.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "../App.css";
-import axios from "axios";
 
 export default function Fxconverter() {
-  var key = "OO3LQyjtTLlIJsljTTnLetLHgKHXv6R0";
-
-  const BASEU_URL = "https://api.exchangeratesapi.io/latest";
+  const [currencyoptions, setCurrencyoptions] = useState([]);
+  const [fromcurrency, setFromcurrency] = useState();
+  const [toCurrency, settoCurrency] = useState();
 
   useEffect(() => {
     var myHeaders = new Headers();
@@ -18,16 +17,25 @@ export default function Fxconverter() {
     };
 
     fetch("https://api.apilayer.com/exchangerates_data/latest?", requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log(result))
+      .then((response) => response.json())
+      .then((data) => {
+        const firstcurrency = Object.keys(data.rates)[0];
+        setCurrencyoptions([data.base, ...Object.keys(data.rates)]);
+        setFromcurrency(data.base);
+        settoCurrency(firstcurrency);
+      })
       .catch((error) => console.log("error", error));
   }, []);
+
+  <Fxconverter currencyoptions={currencyoptions} />;
 
   return (
     <div className="body">
       <input type="number" className="input" />
       <select className="select">
-        <option value="hi">FX</option>
+        {currencyoptions.map((option) => (
+          <option value={option}>{option}</option>
+        ))}
       </select>
     </div>
   );

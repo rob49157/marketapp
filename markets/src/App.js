8d2828d44c95f632721e9b1117a163f3b7d114b8
rob49157 <robertopaipo@hotmail.com>
2022-05-30 20:03:11 -0400

    routes

--- markets/src/App.js
+++ markets/src/App.js
@@ -35,12 +35,12 @@ function App() {
 
       <Navbar className="navbar" bg="success" variant="dark">
 
-        <Navbar.Brand Classname='metalgear' href="/" element={Home}>MarketList</Navbar.Brand>
+        <Navbar.Brand Classname='metalgear' href="/marketapp" element={Home}>MarketList</Navbar.Brand>
         <Nav className="me-auto">
           
-          <Nav.Link href="/Crypto" element={Crypto}>Crypto</Nav.Link>
-          <Nav.Link href="/converter" element={Fxconverter}>Fxconverter</Nav.Link>
-          <Nav.Link href="/Forex" element={Forex}>Forex</Nav.Link>
+          <Nav.Link href="/marketapp/Crypto" element={Crypto}>Crypto</Nav.Link>
+          <Nav.Link href="/marketapp/Forex" element={Forex}>Forex</Nav.Link>
+          <Nav.Link href="/marketapp/converter" element={Fxconverter}>Fxconverter</Nav.Link>
 
 
 
@@ -52,7 +52,7 @@ function App() {
       </Navbar>
 
 
-      <BrowserRouter basename="/" >
+      <BrowserRouter basename="/marketapp" >
         <Routes>
           <Route exact path="/" element={ <Home />} />
           <Route exact path="/Crypto" element={<Crypto  />} />
